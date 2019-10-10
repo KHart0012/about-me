2436b41cdfc0e3bf377395325ff690f4dd058c1e
KHart0012 <34316627+KHart0012@users.noreply.github.com>
2019-10-10 12:24:23 -0500

    Added pokemon API

--- scripts/main.js
+++ scripts/main.js
@@ -23,9 +23,33 @@ function store(){
     localStorage.setItem('Subject: ', subject)
 }
 
+document.querySelector('#poke_sprite').style.visibility = "hidden"
+
+async function get_random_pokemon(){
+    let poke_num = Math.floor((Math.random() * 807) + 1)
+    let pokeURI = 'https://pokeapi.co/api/v2/pokemon/' + poke_num + '/'
+    
+    const response = await fetch(pokeURI)
+    const obj = await response.json()
+
+    let poke_name = obj.forms[0].name
+    let poke_sprite = obj.sprites.front_default
+
+    document.querySelector('#pokemon').innerHTML = poke_name
+    document.querySelector('#poke_sprite').style.visibility = "visible"
+    document.querySelector('#poke_sprite').src = poke_sprite
+}
+
+
+
+if (document.querySelector('#get_pokemon') != null){
+    document.querySelector('#get_pokemon').addEventListener('click', get_random_pokemon)
+}
+
 if (document.querySelector('#submit') != null){
     document.querySelector('#submit').addEventListener('click', find_total_items)
 }
+
 if (document.querySelector('#contact') != null){
     document.querySelector('#contact').addEventListener('click', store)
 }
